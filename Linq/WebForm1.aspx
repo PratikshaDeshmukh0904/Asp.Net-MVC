cbbfefc64ceda227a05f4e5f1b5ea1b02f20adac
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-04-15 00:59:03 -0700

    Linq

---
+++ Linq/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PracticeLinq.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h4>Using Lambda Expression</h4>
+            <asp:GridView runat="server" ID="gridview1"></asp:GridView>
+            <br />
+            <br />
+            <h4>Using Sql like query Expression </h4>
+             <asp:GridView runat="server" ID="gridview2"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Linq/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PracticeLinq
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Using Lambda Expression
+            IEnumerable<Student> queryResult = Student.GetAllStudents().Where(student => student.Gender == "Female");
+            gridview1.DataSource = queryResult;
+            gridview1.DataBind();
+
+            //Using Sql like query Expression 
+            IEnumerable<Student> queryResult1 = from student in Student.GetAllStudents() where student.Gender == "male" select student ;
+            gridview2.DataSource = queryResult1;
+            gridview2.DataBind();
+        }
+    }
+}
