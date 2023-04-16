73ade90b0254045088280d9a9478badf2c61629e
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-04-16 02:00:06 -0700

    Projection Operator in Linq

---
+++ ProjectionOperators/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectionOperators
+{
+    internal class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int AnnualSalary { get; set; }
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>
+            {
+                new Employee
+                {
+                    EmployeeID = 1,
+                    FirstName = "Pratiksha",
+                    LastName = "Deshmukh",
+                    Gender = "Female",
+                    AnnualSalary = 12000
+                },
+                 new Employee
+                {
+                    EmployeeID = 2,
+                    FirstName = "Aarti",
+                    LastName = "Deshmukh",
+                    Gender = "Female",
+                    AnnualSalary = 19000
+                },
+                  new Employee
+                {
+                    EmployeeID = 3,
+                    FirstName = "sonu",
+                    LastName = "Deshmukh",
+                    Gender = "male",
+                    AnnualSalary = 12000
+                },
+                   new Employee
+                {
+                    EmployeeID = 4,
+                    FirstName = "shital",
+                    LastName = "patil",
+                    Gender = "Female",
+                    AnnualSalary = 3000000
+                },
+
+            };
+            return listEmployees;
+        }
+    }
+}
---
+++ ProjectionOperators/Prediction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectionOperators
+{
+    internal class Prediction
+    {
+        static void Main(string[] args)
+        {
+           IEnumerable<int> result = Employee.GetAllEmployees().Select(emp => emp.EmployeeID);
+            foreach(int id in result)
+            {
+                Console.WriteLine(id);
+               
+            }
+
+
+            var result1 = Employee.GetAllEmployees().Select(emp => new { FirstName = emp.FirstName, Gender = emp.Gender });
+            foreach (var v in result1)
+            {
+                Console.WriteLine(v.FirstName+ " - " +v.Gender);
+               
+
+            }
+
+            var result2 = Employee.GetAllEmployees().Select(emp => new { Fullname = emp.FirstName+ " "+emp.LastName, Gender = emp.Gender ,MonthlySalary = emp.AnnualSalary/12});
+            foreach (var v in result2)
+            {
+                Console.WriteLine(v.Fullname + " - " + v.Gender+ " -"+v.MonthlySalary);
+                
+
+            }
+
+            var result3 = Employee.GetAllEmployees().Where(emp =>emp.AnnualSalary > 50000).Select(
+                emp => new 
+                {   
+                    Fullname = emp.FirstName + " " + emp.LastName,
+                    Gender = emp.Gender,
+                    AnnualSalary = emp.AnnualSalary,
+                    Bonus = emp.AnnualSalary * .1
+                }
+                );
+            foreach (var v in result3)
+            {
+                Console.WriteLine(v.Fullname + " - " + v.Gender + " -" + v.AnnualSalary + "-"+v.Bonus);
+                Console.ReadLine();
+
+            }
+        }
+        
+    }
+}
